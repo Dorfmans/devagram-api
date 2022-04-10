429673c4d62f5f6d3589267e94157a87da93ede0
Dorfman <raphaeldorfman@gmail.com>
2022-04-10 18:13:23 -0300

    user data connected to db

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,9 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import type { defaultMessage } from "../../types/defaultMessage";
 import {tokenAuth} from '../../middlewares/tokenAuth';
+import { dbConnection } from "../../middlewares/dbConnection";
+import { userModels } from '../../models/userModels';
 
-const userEndpoint = (req: NextApiRequest, res: NextApiResponse<defaultMessage>) => {
-    return res.status(200).json({message: 'User Successfully Authenticated'});
+const userEndpoint = async (req: NextApiRequest, res: NextApiResponse<defaultMessage | any>) => {
+    
+    try{
+        const { userId } = req?.query;
+        const user = await userModels.findById(userId);
+        user.password = null;
+        return res.status(200).json({user})
+    }catch(e){
+        console.log(e)
+    }
+    return res.status(400).json({error: 'Cannot find user data'})
 }
 
-export default  tokenAuth(userEndpoint);
+export default  tokenAuth(dbConnection(userEndpoint));
