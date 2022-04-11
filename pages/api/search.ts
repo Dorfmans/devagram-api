1b092437db94b6ccda8d8ac4f0170145a1503f89
Dorfman <raphaeldorfman@gmail.com>
2022-04-10 22:23:26 -0300

    Search endpoint updated

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -5,15 +5,25 @@ import { dbConnection } from "../../middlewares/dbConnection";
 import { userModels } from '../../models/userModels';
 // import nc from 'next-connect';
 
-const searchEndpoint = async (req: NextApiRequest, res: NextApiResponse<defaultMessage | any>) => {
+const searchEndpoint = async (req: NextApiRequest, res: NextApiResponse<defaultMessage | any >) => {
     try{
         if(req.method === 'GET'){
+            if(req?.query?.id){
 
-            const { search } = req.query;
-            
+                const result = await userModels.findById(req?.query?.id);
+                result.password = null
+
+                if(!result){
+                    return res.status(400).json({error: 'User not found'});
+                }
+                return res.status(200).json(result);}
+            else{
+                const { search } = req.query;
+
             if(!search || search.length< 2){
-                return res.status(400).json({error: 'Insert a valid data'})
+                return res.status(400).json({error: 'Insert a valid data'});
             }
+            
             const result = await userModels
             .find({
                 $or:[
@@ -24,16 +34,16 @@ const searchEndpoint = async (req: NextApiRequest, res: NextApiResponse<defaultM
                         $regex: search,
                         $options: 'i'}},
                 ]
-            })
-                return res.status(200).json({result})
-            }
-        return res.status(405).json({error: 'Invalid Method'})
+            });
+            result.forEach(e => e.password = null);
+            return res.status(200).json(result);
+            }}
 
-
+        return res.status(405).json({error: 'Invalid Method'});
     }catch(e){
         console.log(e)
-        return res.status(500).json({error: 'Cannot search user'})
+        return res.status(500).json({error: 'Cannot search user'});
     }
 }
 
-export default tokenAuth(dbConnection(searchEndpoint))
+export default tokenAuth(dbConnection(searchEndpoint));
